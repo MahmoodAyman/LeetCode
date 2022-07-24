19b52517fb01552839ce0655cb8eea2991c81813
Mahmoud Ayman <56787182+MahmoodAyman@users.noreply.github.com>
2022-07-24 16:19:33 +0200

    Time: 1402 ms (5.01%), Space: 13 MB (53.26%) - LeetHub

--- 611-valid-triangle-number/611-valid-triangle-number.cpp
+++ 611-valid-triangle-number/611-valid-triangle-number.cpp
@@ -1,32 +1,20 @@
 class Solution {
 public:
-    int BS(vector<int> &v, int start, int val) {
-        int left = start;
-        int right = (int) v.size() - 1;
-        int pos = v.size();
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (v[mid] < val) {
-                left = mid + 1; 
-            } else {
-                right = mid - 1;
-                pos = mid;
-            }
-        }
-        return pos;
-    }
+int triangleNumber(vector<int> &nums) {
+    sort(nums.begin(), nums.end());
+    int counter = 0;
+    for (int i = 0; i < (int) nums.size(); i++) {
+             int k = i + 2;
+        for (int j = i + 1; j < (int) nums.size()&&nums[i]!=0; j++) {
+            int valid_pos = nums.size();
+            auto iterator = lower_bound(nums.begin() + k, nums.end(), nums[i] + nums[j]);
+            if (iterator != nums.end()) {
+                valid_pos = iterator - nums.begin();
 
-    int triangleNumber(vector<int> &nums) {
-        int counter = 0;
-        sort(nums.begin(),nums.end());
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                int k = j + 1;// my start value
-                int first_idx_equal = BS(nums, k, nums[i] + nums[j]);
-                counter += first_idx_equal - j - 1;
             }
+            counter+=valid_pos-j-1;
         }
-
-        return counter;
     }
+    return counter;
+}
 };
