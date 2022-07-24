475f0a944e2db95efdea623240ac9308099d95ef
Mahmoud Ayman <56787182+MahmoodAyman@users.noreply.github.com>
2022-07-24 15:37:30 +0200

    Time: 156 ms (23.64%), Space: 32.2 MB (9.37%) - LeetHub

--- 436-find-right-interval/436-find-right-interval.cpp
+++ 436-find-right-interval/436-find-right-interval.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-  vector<int> findRightInterval(vector<vector<int>>& intervals) {
+vector<int> findRightInterval(vector<vector<int>>& intervals) {
     vector<vector<int>>starts;
+    vector<int>ans(intervals.size(),-1);
     for(int i=0;i<intervals.size();i++){
         starts.push_back({intervals[i][0],i});
     }
     sort(starts.begin(),starts.end());
-    vector<int>ans((int)intervals.size(),-1);
     for(int i=0;i<intervals.size();i++){
-        int left=0,right=(int)starts.size()-1;
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            if(starts[mid][0]>=intervals[i][1]){
-                ans[i]=starts[mid][1];
-                right=mid-1;
-            }
-            else {
-                left=mid+1;
-            }
+        vector<int>val{intervals[i][1],0};
+        auto iterator= lower_bound(starts.begin(),starts.end(),val);
+        if(iterator!=starts.end()){
+            int idx=iterator-starts.begin();
+            ans[i]=starts[idx][1];
+
         }
     }
     return ans;
