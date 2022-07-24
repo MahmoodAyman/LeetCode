6d3bbca43df976c12d06aaa7bfea42bd155de1e6
Mahmoud Ayman <56787182+MahmoodAyman@users.noreply.github.com>
2022-07-24 22:54:38 +0200

    Time: 286 ms (40.23%), Space: 63.3 MB (64.74%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    void Fast() {
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
     bool possible(vector<int> &v, int mid, int m, int k) {
         int adj_flowers = 0, bouquets = 0;
         for (int i = 0; i < v.size(); i++) {
@@ -17,6 +22,7 @@ public:
     }
 
     int minDays(vector<int> &bloomDay, int m, int k) {
+        Fast();
         if (m * k > bloomDay.size())return -1;
         int ans = -1, left = 1, right = *max_element(bloomDay.begin(), bloomDay.end());
         while (left <= right) {
