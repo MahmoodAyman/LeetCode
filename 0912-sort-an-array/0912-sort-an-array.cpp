4d0226499d90c90c01e76005fb3b049ed6835136
Mahmoud Ayman <56787182+MahmoodAyman@users.noreply.github.com>
2023-11-15 09:43:04 +0200

    Time: 91 ms (93.81%), Space: 73.5 MB (48.77%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,15 +1,23 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        vector<int>ans;
-        map<int,int>mp;
-        for(auto &it:nums)
-            mp[it]++;
-        for(auto &it:mp){
-            while(it.second--){
-                ans.emplace_back(it.first);
-            }
-        }
+     vector<int>ans;
+        int mx = -100000;
+     for(auto &it:nums){
+         if(it<0){
+             mx = max(mx,it*-1);
+         }
+         else mx= max(mx,it);
+     }
+     vector<int>freq(mx*2+2);
+     for(auto &it:nums){
+         freq[it + mx]++;
+     }
+     for(int i = 0;i<mx*2+2;i++){
+         while(freq[i]--){
+            ans.emplace_back(i-mx);
+         }
+     }
         return ans;
     }
 };
