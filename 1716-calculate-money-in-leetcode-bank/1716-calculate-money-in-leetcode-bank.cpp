7563d022603aa0a7a5326f575427ce78ae414688
Mahmoud Ayman <56787182+MahmoodAyman@users.noreply.github.com>
2023-12-06 15:08:51 +0200

    Time: 0 ms (100.00%), Space: 6.2 MB (98.95%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int ans = 0;
-        int coins = 1;
         int weeks = n / 7;
-        while(weeks--){
-            for(int i = 0; i< 7; ++i){
-                ans+=(coins + i);
-            }
-            coins++;
-        }
-        int rem = n % 7;
-        while(rem--){
-            ans+=coins;
-            coins++;
-        }
+        int ans = weeks * 28 + ((weeks * (weeks - 1)) / 2) * 7;
+        int remDays = n % 7;
+        ans += (remDays * (remDays + 1)) / 2 + (weeks * remDays);
         return ans;
     }
 };
