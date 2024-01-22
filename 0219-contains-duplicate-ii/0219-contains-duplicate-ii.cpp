f7991e77c948170f24e947f501b13dd4419866b7
Mahmoud Ayman <56787182+MahmoodAyman@users.noreply.github.com>
2024-01-22 19:50:11 +0200

    Time: 132 ms (61.77%), Space: 76.1 MB (85.73%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,15 +1,16 @@
-class Solution {
+class Solution
+{
 public:
-    
-bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    unordered_set<int>st;
-    for(int i = 0 ; i < nums.size(); i++){
-        if(st.count(nums[i]))
-            return true;
-        st.insert(nums[i]);
-        if(st.size() > k)
-            st.erase(nums[i - k]);
+    bool containsNearbyDuplicate(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        unordered_set<int> st;
+        for (int i = 0; i < n; i++)
+        {
+            if(st.count(nums[i]))return true;
+            st.insert(nums[i]);
+            if(st.size() > k)st.erase(nums[i - k]);
+        }
+        return false;
     }
-    return false;
-}
 };
